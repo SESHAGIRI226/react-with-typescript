a062e531cab0746eb4dfb00ffff94477b1598e8c
Pentapati Seshagiri <Pentapati.Seshagiri@ivycomptech.com>
2024-05-13 18:49:42 +0530

    react with typescript

---
+++ code/08 Practice Project/02 Finished/src/components/Sessions/UpcomingSessions.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef } from 'react';
+
+import Modal, { type ModalHandle } from '../UI/Modal.tsx';
+import UpcomingSession from './UpcomingSession.tsx';
+import { useSessionsContext } from '../../store/sessions-context.tsx';
+import Button from '../UI/Button.tsx';
+
+type UpcomingSessionsProps = {
+  onClose: () => void; // onClose is accepted to "tell" the parent component that the UpcomingSessions component should be removed from the DOM
+};
+
+export default function UpcomingSessions({ onClose }: UpcomingSessionsProps) {
+  const modal = useRef<ModalHandle>(null);
+  const sessionsCtx = useSessionsContext();
+
+  console.log(sessionsCtx);
+
+  // useEffect is used to open the Modal via its exposed `open` method when the component is mounted
+  useEffect(() => {
+    if (modal.current) {
+      modal.current.open();
+    }
+  }, []);
+
+  function handleCancelSession(sessionId: string) {
+    sessionsCtx.cancelSession(sessionId);
+  }
+
+  const hasSessions = sessionsCtx.upcomingSessions.length > 0;
+
+  return (
+    <Modal ref={modal} onClose={onClose}>
+      <h2>Upcoming Sessions</h2>
+      {hasSessions && (
+        <ul>
+          {sessionsCtx.upcomingSessions.map((session) => (
+            <li key={session.id}>
+              <UpcomingSession
+                session={session}
+                onCancel={() => handleCancelSession(session.id)}
+              />
+            </li>
+          ))}
+        </ul>
+      )}
+      {!hasSessions && <p>No upcoming sessions.</p>}
+      <p className="actions">
+        <Button onClick={onClose}>Close</Button>
+      </p>
+    </Modal>
+  );
+}
